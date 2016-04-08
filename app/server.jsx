00c97e3d05d5cd8c067ef31d8663a32592bc7252
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-09 04:16:54 +0800

    Add Open Sans fonts (Google Fonts)

--- app/server.jsx
+++ app/server.jsx
@@ -29,6 +29,8 @@ function renderFullPage( html, initialState ) {
   <head>
     <meta charset=utf-8 />
     <title>${ initialState.info.name }</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&subset=latin%2Clatin-ext" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans%3A300%2C400%2C700%2C700italic%2C400italic%2C300italic%7COpen+Sans+Condensed%3A700%2C700italic&subset=latin%2Clatin-ext" />
     <link rel="stylesheet" href="/assets/genericons/genericons/genericons.css" />
     <link rel="stylesheet" href="/assets/style.css" />
   </head>
