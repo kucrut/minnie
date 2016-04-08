7ac3075d0a379bfad2df103698ea0bdefe77ae47
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-09 00:03:32 +0800

    Fix social menu item's markup

--- app/components/SocialMenu.jsx
+++ app/components/SocialMenu.jsx
@@ -10,11 +10,15 @@ class SocialMenu extends Component {
 		return (
 			<div className="social-links">
 				<ul id="menu-social" className="menu">
-					{ this.props.items.map( item => (
-						<li className="menu-item">
-							<a href={ item.url }><span className="screen-reader-text">{ item.title }</span></a>
-						</li>
-					))}
+					{ this.props.items.map( item => {
+						const id = `menu-item-${item.ID}`
+
+						return (
+							<li key={ id } id={ id } className={ `menu-item ${id}` }>
+								<a href={ item.url }><span className="screen-reader-text">{ item.title }</span></a>
+							</li>
+						)
+					}) }
 				</ul>
 			</div>
 		)
