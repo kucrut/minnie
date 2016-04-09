794a1b393dc1f4e4aec34df9d1043e8987a7aa59
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-10 00:59:14 +0800

    Store: Taxonomies

---
+++ app/actions/taxonomies.js
@@ -0,0 +1,13 @@
+import { polyfill } from 'es6-promise';
+import request from 'axios'
+import { GET_TAXONOMIES } from 'constants/index'
+
+
+polyfill();
+
+export function fetchTaxonomies() {
+	return {
+		type: GET_TAXONOMIES,
+		promise: request({ url: '/wp/v2/taxonomies' })
+	}
+}
