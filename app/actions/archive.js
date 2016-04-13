d4d079848119c56f673681687ae4a275ef2f2dce
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-13 14:22:39 +0800

    Decouple axios call from `fetchArchive()`

--- app/actions/archive.js
+++ app/actions/archive.js
@@ -1,18 +1,22 @@
+import axios from 'axios'
 import { polyfill } from 'es6-promise'
-import request from 'axios'
 import { GET_ARCHIVE } from 'constants/index'
 import { normalizeParams } from 'helpers.js'
-import axios from 'axios'
+
 
 polyfill();
+
+function makeArchiveRequest( params ) {
+	return axios({
+		method: 'get',
+		url: '/wp/v2/posts',
+		params: normalizeParams( params )
+	})
+}
 
 export function fetchArchive( params = {} ) {
 	return {
 		type: GET_ARCHIVE,
-		promise: axios({
-			method: 'get',
-			url: `/wp/v2/posts`,
-			params: normalizeParams( params )
-		})
+		promise: makeArchiveRequest( params )
 	}
 }
