0d21318dfb5e6a728d2a81c377ebe46972135718
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-15 02:55:01 +0800

    EntryTitle: Cleanup

--- app/components/EntryTitle.jsx
+++ app/components/EntryTitle.jsx
@@ -1,26 +1,28 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
 
-const EntryTitle = props => {
-	const title = { __html: props.title }
+export default class EntryTitle extends Component {
+	static propTypes = {
+		link: PropTypes.string,
+		title: PropTypes.string.isRequired,
+		isSingle: PropTypes.bool.isRequired
+	}
 
-	if ( props.isSingle ) {
-		return (
-			<h1 className="entry-title" dangerouslySetInnerHTML={ title } />
-		)
-	} else {
-		return (
-			<h1 className="entry-title">
-				<Link to={ props.link } dangerouslySetInnerHTML={ title } />
-			</h1>
-		)
+	render() {
+		const { isSingle, link } = this.props
+		const title = { __html: this.props.title }
+
+		if ( isSingle ) {
+			return (
+				<h1 className="entry-title" dangerouslySetInnerHTML={ title } />
+			)
+		} else {
+			return (
+				<h1 className="entry-title">
+					<Link to={ link } dangerouslySetInnerHTML={ title } />
+				</h1>
+			)
+		}
 	}
 }
-
-EntryTitle.propTypes = {
-	title: PropTypes.string.isRequired,
-	isSingle: PropTypes.bool.isRequired
-}
-
-export default EntryTitle
