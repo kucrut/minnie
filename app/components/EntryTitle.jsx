a7430c802ba7cd9d81c96b43c18ba21d88088aef
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-15 02:48:22 +0800

    EntryTitle: Display post title as is
    It's already escaped by WordPress

--- app/components/EntryTitle.jsx
+++ app/components/EntryTitle.jsx
@@ -4,14 +4,16 @@ import { stripApiHost } from 'helpers.js'
 
 
 const EntryTitle = props => {
+	const title = { __html: props.title }
+
 	if ( props.isSingle ) {
 		return (
-			<h1 className="entry-title">{ props.title }</h1>
+			<h1 className="entry-title" dangerouslySetInnerHTML={ title } />
 		)
 	} else {
 		return (
 			<h1 className="entry-title">
-				<Link to={ stripApiHost( props.link ) }>{ props.title }</Link>
+				<Link to={ stripApiHost( props.link ) } dangerouslySetInnerHTML={ title } />
 			</h1>
 		)
 	}
