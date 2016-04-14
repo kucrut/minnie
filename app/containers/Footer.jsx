1d2af0edf3b649a6b51d6d7cfc42d9415fa58705
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-15 03:45:06 +0800

    Update footer

--- app/containers/Footer.jsx
+++ app/containers/Footer.jsx
@@ -4,11 +4,9 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<footer id="colophon" className="site-footer" role="contentinfo">
-			<div className="site-info">
-				<a href="http://wordpress.org/">Proudly powered by WordPress</a>
-				<span className="sep"> | </span>
-				Based on Minnow Theme by <a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>.
-			</div>
+			<p className="site-info">
+				Proudly powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://github.com/kucrut/minnie">Minnie</a>. Based on <a href="https://minnowdemo.wordpress.com/">Minnow</a> by <a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>.
+			</p>
 		</footer>
 	)
 }
