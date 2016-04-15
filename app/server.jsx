c8bcf43b2a09fc03e15c064572ed3082cb021acd
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-16 03:04:45 +0800

    Add meta viewport

--- app/server.jsx
+++ app/server.jsx
@@ -28,6 +28,7 @@ function renderFullPage( html, initialState ) {
 <html>
   <head>
     <meta charset=utf-8 />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     ${ head.title.toString() }
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic|Open+Sans:400,400italic,600,600italic,700,700italic&subset=latin,greek,cyrillic,vietnamese,cyrillic-ext,latin-ext'>
     <link rel="stylesheet" href="/assets/genericons/genericons/genericons.css" />
