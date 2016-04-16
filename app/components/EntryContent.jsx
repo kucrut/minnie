05986e112d265f8fe3c8dd8fd8373233fac92137
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-16 21:38:56 +0800

    Remove click handle on EntryContent
    `home_url()` is already stripped from the content by the backend plugin.

--- app/components/EntryContent.jsx
+++ app/components/EntryContent.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
-import { stripApiHost, isInternalLink } from 'helpers.js'
 
 
 class EntryContent extends Component {
@@ -11,26 +10,12 @@ class EntryContent extends Component {
 		dispatch: PropTypes.func.isRequired
 	}
 
-	constructor( props ) {
-		super( props )
-		this.onClick = this.onClick.bind( this )
-	}
-
-	onClick(e) {
-		const anchor = e.target.closest('a')
-
-		if ( anchor && isInternalLink( anchor.href ) ) {
-			e.preventDefault()
-			this.props.dispatch( push( stripApiHost( anchor.href ) ) )
-		}
-	}
-
 	render() {
 		const { content, wrapClass } = this.props
 		let divClass = wrapClass || 'entry-content'
 
 		return (
-			<div className={ divClass } dangerouslySetInnerHTML={{ __html: content }} onClick={ this.onClick } />
+			<div className={ divClass } dangerouslySetInnerHTML={{ __html: content }} />
 		)
 	}
 }
