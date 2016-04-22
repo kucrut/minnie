5711cdf77246e2aea72e30f915dd18abcd07898b
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-22 22:23:20 +0800

    ArchiveNavigation: Support single post navigation

--- app/components/ArchiveNavigation.jsx
+++ app/components/ArchiveNavigation.jsx
@@ -1,20 +1,25 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import he from 'he'
 
 
 export default class ArchiveNavigation extends Component {
 	static propTypes = {
+		isSingle: PropTypes.bool,
 		prevLink: PropTypes.string,
-		nextLink: PropTypes.string
+		prevText: PropTypes.string,
+		nextLink: PropTypes.string,
+		nextText: PropTypes.string
 	}
 
 	renderPrevLink() {
-		const { prevLink } = this.props
+		const { prevLink, prevText } = this.props
+		let text = prevText ? prevText : 'Older posts'
 
 		if ( prevLink ) {
 			return (
 				<div className="nav-previous">
-					<Link to={ prevLink }><span className="meta-nav">←</span> Older posts</Link>
+					<Link to={ prevLink }><span className="meta-nav">←</span> { he.decode( text ) }</Link>
 				</div>
 			)
 		}
@@ -22,24 +27,34 @@ export default class ArchiveNavigation extends Component {
 
 
 	renderNextLink() {
-		const { nextLink } = this.props
+		const { nextLink, nextText } = this.props
+		let text = nextText ? nextText : 'Newer posts'
 
 		if ( nextLink ) {
 			return (
 				<div className="nav-next">
-					<Link to={ nextLink }>Newer posts <span className="meta-nav">→</span></Link>
+					<Link to={ nextLink }>{ he.decode( text ) } <span className="meta-nav">→</span></Link>
 				</div>
 			)
 		}
 	}
 
 	render() {
-		const { prevLink, nextLink } = this.props
+		const { isSingle, prevLink, nextLink } = this.props
+		let navClass, navTitle
+
+		if ( isSingle ) {
+			navClass = 'navigation post-navigation'
+			navTitle = 'Post navigation'
+		} else {
+			navClass = 'navigation paging-navigation'
+			navTitle = 'Posts navigation'
+		}
 
 		if ( prevLink || nextLink ) {
 			return (
-				<nav className="navigation paging-navigation" role="navigation">
-					<h1 className="screen-reader-text">Posts navigation</h1>
+				<nav className={ navClass } role="navigation">
+					<h1 className="screen-reader-text">{ navTitle }</h1>
 					<div className="nav-links">
 						{ this.renderPrevLink() }
 						{ this.renderNextLink() }
