016c12e6f1768f7ffd57816c61ad5000682b3b8a
Dzikri Aziz <kvcrvt@gmail.com>
2016-04-22 17:21:50 +0800

    Click event handler inside EntryContent

--- app/components/EntryContent.jsx
+++ app/components/EntryContent.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
+import closest from 'dom-closest'
 
 
 class EntryContent extends Component {
@@ -10,12 +11,34 @@ class EntryContent extends Component {
 		dispatch: PropTypes.func.isRequired
 	}
 
+	constructor( props ) {
+		super( props )
+		this.handleClick = this.handleClick.bind( this )
+	}
+
+	handleClick( e ) {
+		let anchor = closest( e.target, 'a' )
+
+		if ( ! anchor ) {
+			return
+		}
+
+		if ( anchor.hostname === location.hostname ) {
+			e.preventDefault()
+			this.props.dispatch( push( anchor.pathname ) )
+		}
+	}
+
 	render() {
 		const { content, wrapClass } = this.props
 		let divClass = wrapClass || 'entry-content'
 
 		return (
-			<div className={ divClass } dangerouslySetInnerHTML={{ __html: content }} />
+			<div
+				className={ divClass }
+				onClick={ this.handleClick }
+				dangerouslySetInnerHTML={{ __html: content }}
+			/>
 		)
 	}
 }
