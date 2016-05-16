809d222225b979627e21d740aad72076b9e960e1
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-16 14:26:54 +0800

    Lint server/

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
-var express = require('express');
-var fs = require('fs');
-var webpack = require('webpack');
-var app = express();
+var express = require( 'express' );
+var webpack = require( 'webpack' );
+var app     = express();
 
 var isDev = process.env.NODE_ENV === 'development';
 
-if (isDev) {
-  var config = require('../webpack/webpack.config.dev-client.js');
-  var compiler = webpack(config);
+if ( isDev ) {
+	var config   = require( '../webpack/webpack.config.dev-client.js' );
+	var compiler = webpack( config );
 
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }));
+	app.use( require( 'webpack-dev-middleware' )( compiler, {
+		noInfo: true,
+		publicPath: config.output.publicPath
+	}) );
 
-  app.use(require('webpack-hot-middleware')(compiler));
+	app.use( require( 'webpack-hot-middleware' )( compiler ) );
 }
 
 // Bootstrap application settings
-require('./config/express')(app);
-require('./config/routes')(app);
+require( './config/express' )( app );
+require( './config/routes' )( app );
 
-app.listen(app.get('port'));
+app.listen( app.get( 'port' ) );
