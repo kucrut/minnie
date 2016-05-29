02c0e76bc7b5b9bb38ad231a1517291fe29cbd30
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-30 05:35:34 +0800

    Fix footer text spacing

--- app/containers/Footer.jsx
+++ app/containers/Footer.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-
 export default function Footer() {
 	return (
 		<footer id="colophon" className="site-footer" role="contentinfo">
-			<p className="site-info">
-				Proudly powered by <a href="https://wordpress.org/">WordPress</a>
-				&amp; <a href="https://github.com/kucrut/minnie">Minnie</a>.
-				Based on <a href="https://minnowdemo.wordpress.com/">Minnow</a>
-				by <a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>.
-			</p>
+			<p className="site-info">Proudly powered by <a href="https://wordpress.org/">WordPress</a> &amp; <a href="https://github.com/kucrut/minnie">Minnie</a>. Based on <a href="https://minnowdemo.wordpress.com/">Minnow</a> by <a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>.</p>
 		</footer>
 	);
 }
