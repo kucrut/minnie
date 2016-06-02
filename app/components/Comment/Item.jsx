8ba91421b6e711810dac31e3b18f9578ed7ac4d2
Dzikri Aziz <kvcrvt@gmail.com>
2016-06-02 21:33:44 +0800

    Add "View replies" link on CommentMeta

--- app/components/Comment/Item.jsx
+++ app/components/Comment/Item.jsx
@@ -1,30 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import CommentContent from 'components/Comment/Content';
 import CommentAuthor from 'components/Comment/Author';
 import CommentMeta from 'components/Comment/Meta';
 
-export default function Comment({ comment }) {
-	const {
-		date,
-		link,
-		date_formatted: dateFormatted,
-		author_name: authorName,
-		author_avatar_urls: avatarUrls
-	} = comment;
+export default class Comment extends Component {
+	static propTypes = {
+		comment: PropTypes.object.isRequired
+	}
 
-	return (
-		<li id={ `comment-${comment.id}` } className="comment">
-			<article className="comment-body">
-				<div className="comment-content">
-					<CommentAuthor authorName={ authorName } avatarUrl={ avatarUrls[ '48' ] } />
-					<CommentContent content={ comment.content.rendered } />
-				</div>
-				<CommentMeta date={ date } link={ link } dateFormatted={ dateFormatted } />
-			</article>
-		</li>
-	);
+	constructor( props ) {
+		super( props );
+		this.handleViewReplies = this.handleViewReplies.bind( this );
+	}
+
+	handleViewReplies() {
+		// TODO.
+	}
+
+	render() {
+		const {
+			id,
+			date,
+			link,
+			content,
+			date_formatted: dateFormatted,
+			author_name: authorName,
+			author_avatar_urls: avatarUrls
+		} = this.props.comment;
+
+		return (
+			<li id={ `comment-${id}` } className="comment">
+				<article className="comment-body">
+					<div className="comment-content">
+						<CommentAuthor authorName={ authorName } avatarUrl={ avatarUrls[ '48' ] } />
+						<CommentContent content={ content.rendered } />
+					</div>
+					<CommentMeta
+						date={ date }
+						link={ link }
+						dateFormatted={ dateFormatted }
+						handleViewReplies={ this.handleViewReplies }
+					/>
+				</article>
+			</li>
+		);
+	}
 }
-
-Comment.propTypes = {
-	comment: PropTypes.object.isRequired
-};
--- app/components/Comment/Meta.jsx
+++ app/components/Comment/Meta.jsx
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react';
 
-export default function CommentMeta({ link, date, dateFormatted }) {
+export default function CommentMeta( props ) {
+	const {
+		link,
+		date,
+		dateFormatted,
+		handleViewReplies
+	} = props;
+
 	return (
 		<div className="comment-metadata">
 			<a href={ link }>
 				<time dateTime={ date }>{ dateFormatted }</time>
 			</a>
+			<a className="comment-view-replies-link" onClick={ handleViewReplies }>View replies</a>
 			<span className="reply">
 				<a href="#" className="comment-reply-link" rel="nofollow">Reply</a>
 			</span>
@@ -14,7 +22,8 @@ export default function CommentMeta({ link, date, dateFormatted }) {
 }
 
 CommentMeta.propTypes = {
-	date:          PropTypes.string.isRequired,
-	link:          PropTypes.string.isRequired,
-	dateFormatted: PropTypes.string.isRequired
+	date:              PropTypes.string.isRequired,
+	link:              PropTypes.string.isRequired,
+	dateFormatted:     PropTypes.string.isRequired,
+	handleViewReplies: PropTypes.func.isRequired
 };
