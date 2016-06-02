5c566e81855def16756194caa11b9ffab6bbdecc
Dzikri Aziz <kvcrvt@gmail.com>
2016-06-03 04:42:47 +0800

    Make LoadMoreButton reusable everywhere

--- app/components/LoadMoreButton.jsx
+++ app/components/LoadMoreButton.jsx
@@ -1,30 +1,53 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
-export default function LoadMoreButton( props ) {
-	const { text, onClick, hasMore, isFetching } = props;
-
-	if ( ! hasMore ) {
-		return null;
+export default class LoadMoreButton extends Component {
+	static defaultProps = {
+		text:   'Load More',
+		params: {}
 	}
 
-	const iconClass = classNames({
-		genericon:            true,
-		'animate-spin':       isFetching,
-		'genericon-refresh':  isFetching,
-		'genericon-download': ! isFetching
-	});
+	static propTypes = {
+		onClick:    PropTypes.func.isRequired,
+		hasMore:    PropTypes.bool.isRequired,
+		isFetching: PropTypes.bool.isRequired,
+		text:       PropTypes.string,
+		params:     PropTypes.object
+	}
 
-	return (
-		<div className="load-more">
-			<a onClick={ onClick }><i className={ iconClass } aria-hidden="true" /> { text }</a>
-		</div>
-	);
+	constructor( props ) {
+		super( props );
+		this.handleClick = this.handleClick.bind( this );
+	}
+
+	handleClick() {
+		const { onClick, params, isFetching } = this.props;
+
+		if ( ! isFetching ) {
+			onClick( params );
+		}
+	}
+
+	render() {
+		const { text, hasMore, isFetching } = this.props;
+
+		if ( ! hasMore ) {
+			return null;
+		}
+
+		const iconClass = classNames({
+			genericon:            true,
+			'animate-spin':       isFetching,
+			'genericon-refresh':  isFetching,
+			'genericon-download': ! isFetching
+		});
+
+		return (
+			<div className="load-more">
+				<a onClick={ this.handleClick }>
+					<i className={ iconClass } aria-hidden="true" />{ text }
+				</a>
+			</div>
+		);
+	}
 }
-
-LoadMoreButton.propTypes = {
-	text:       PropTypes.string.isRequired,
-	onClick:    PropTypes.func.isRequired,
-	hasMore:    PropTypes.bool.isRequired,
-	isFetching: PropTypes.bool.isRequired
-};
