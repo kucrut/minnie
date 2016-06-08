1561a97a948990c38485e34b34f94ab0424d5f93
Dzikri Aziz <kvcrvt@gmail.com>
2016-06-09 02:28:36 +0800

    CommentMeta: Convert to class

--- app/components/Comment/Meta.jsx
+++ app/components/Comment/Meta.jsx
@@ -1,39 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import CommentAuthor from 'components/Comment/Author';
 import CommentDate from 'components/Comment/Date';
 import CommentReplyLink from 'components/Comment/ReplyLink';
 
 // TODO: retina avatar image.
-export default function CommentMeta( props ) {
-	const {
-		authorUrl, authorName, avatarUrl,
-		link, date, dateFormatted,
-		allowReplies, onClickReply
-	} =  props;
+export default class CommentMeta extends Component {
+	static propTypes = {
+		link:          PropTypes.string.isRequired,
+		date:          PropTypes.string.isRequired,
+		avatarUrl:     PropTypes.string.isRequired,
+		authorUrl:     PropTypes.string.isRequired,
+		authorName:    PropTypes.string.isRequired,
+		dateFormatted: PropTypes.string.isRequired,
+		allowReplies:  PropTypes.bool.isRequired,
+		onClickReply:  PropTypes.func.isRequired
+	}
 
-	const replyLinkEl = allowReplies ? ( <CommentReplyLink onClick={ onClickReply } /> ) : null;
+	renderReplyLink() {
+		const { allowReplies, onClickReply } = this.props;
 
-	return (
-		<footer className="comment-meta">
-			<div className="comment-author vcard">
-				<p>
-					<img className="avatar" src={ avatarUrl } alt={ `${authorName}'s avatar` } />
-					<CommentAuthor authorName={ authorName } authorUrl={ authorUrl } />
-					{ replyLinkEl }
-				</p>
-				<CommentDate link={ link } date={ date } dateFormatted={ dateFormatted } />
-			</div>
-		</footer>
-	);
+		if ( ! allowReplies ) {
+			return null;
+		}
+
+		return (
+			<CommentReplyLink onClick={ onClickReply } />
+		);
+	}
+	render() {
+		const { authorUrl, authorName, avatarUrl, link, date, dateFormatted } = this.props;
+
+		return (
+			<footer className="comment-meta">
+				<div className="comment-author vcard">
+					<p>
+						<img className="avatar" src={ avatarUrl } alt={ `${authorName}'s avatar` } />
+						<CommentAuthor authorName={ authorName } authorUrl={ authorUrl } />
+						{ this.renderReplyLink() }
+					</p>
+					<p>
+						<CommentDate link={ link } date={ date } dateFormatted={ dateFormatted } />
+					</p>
+				</div>
+			</footer>
+		);
+	}
 }
-
-CommentMeta.propTypes = {
-	link:          PropTypes.string.isRequired,
-	date:          PropTypes.string.isRequired,
-	avatarUrl:     PropTypes.string.isRequired,
-	authorUrl:     PropTypes.string.isRequired,
-	authorName:    PropTypes.string.isRequired,
-	dateFormatted: PropTypes.string.isRequired,
-	allowReplies:  PropTypes.bool.isRequired,
-	onClickReply:  PropTypes.func.isRequired
-};
