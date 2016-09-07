e1c4176bb8cd455610ee2e4fbc11b49ae58dd9fa
Dzikri Aziz <kvcrvt@gmail.com>
2016-09-08 07:15:41 +0800

    Actions: Add openGallery() and closeGallery()

--- app/actions/galleries.js
+++ app/actions/galleries.js
@@ -1,4 +1,10 @@
-import { GALLERY_ADD, GALLERY_RESET } from 'constants/index';
+import {
+	GALLERY_ADD,
+	GALLERY_RESET,
+	GALLERY_OPEN,
+	GALLERY_CLOSE
+} from 'constants/index';
+import closest from 'dom-closest';
 
 export function resetGallery() {
 	return dispatch => {
@@ -12,5 +18,26 @@ export function addGallery( gallery ) {
 			type: GALLERY_ADD,
 			gallery
 		});
+	};
+}
+
+export function openGallery( itemEl ) {
+	const galleryEl = closest( itemEl, 'div.gallery' );
+	const galleryId = galleryEl.getAttribute( 'id' );
+	const allItems = galleryEl.querySelectorAll( '.gallery-item' );
+	const itemIndex = Array.prototype.indexOf.call( allItems, itemEl );
+
+	return dispatch => {
+		dispatch({
+			type:  GALLERY_OPEN,
+			id:    galleryId,
+			index: itemIndex
+		});
+	};
+}
+
+export function closeGallery() {
+	return dispatch => {
+		dispatch({ type: GALLERY_CLOSE });
 	};
 }
