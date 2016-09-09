d86ccddc95ad5f45ccc41b53ee3c47cb6b57b519
Dzikri Aziz <kvcrvt@gmail.com>
2016-09-10 00:37:00 +0800

    Gallery: Introduce createGalleryItem()
    This is decoupled from createGallery()

--- app/misc/galleries.js
+++ app/misc/galleries.js
@@ -1,3 +1,35 @@
+function createGalleryItem( imgEl, origImgUrl, title = '' ) {
+	const msrc  = imgEl.getAttribute( 'src' );
+	const sizes = imgEl.getAttribute( 'srcset' ).split( ', ' ).slice( -2 );
+	const item = [];
+
+	for ( let s = 0; s < sizes.length; ++s ) {
+		const srcW = sizes[ s ].split( ' ' );
+		const src = srcW[ 0 ];
+		const size = title ? { title } : {};
+
+		if ( src === origImgUrl ) {
+			item.push( Object.assign( size, {
+				src: origImgUrl,
+				w:   imgEl.getAttribute( 'data-ow' ),
+				h:   imgEl.getAttribute( 'data-oh' ),
+				msrc
+			}) );
+
+			continue;
+		}
+
+		const w = srcW[ 1 ].replace( 'w', '' );
+		const xPos = src.lastIndexOf( 'x' );
+		const dotPos = src.lastIndexOf( '.' );
+		const h = src.slice( ( xPos + 1 ), dotPos );
+
+		item.push( Object.assign( size, { msrc, src, w, h }) );
+	}
+
+	return item;
+}
+
 export default function createGallery( el ) {
 	const itemEls = el.querySelectorAll( '.gallery-item' );
 	let gallery = {
@@ -6,14 +38,11 @@ export default function createGallery( el ) {
 	};
 	let showCaption = false;
 
-	for ( let t = 0; t < itemEls.length; ++t ) {
-		const itemEl = itemEls[ t ];
+	for ( let i = 0; i < itemEls.length; ++i ) {
+		const itemEl = itemEls[ i ];
+		const captionEl = itemEl.querySelector( 'figcaption' );
 		const origImgUrl = itemEl.querySelector( 'a' ).getAttribute( 'href' );
 		const imgEl = itemEl.querySelector( 'img' );
-		const msrc  = imgEl.getAttribute( 'src' );
-		const sizes = imgEl.getAttribute( 'srcset' ).split( ', ' ).slice( -2 );
-		const captionEl = itemEl.querySelector( 'figcaption' );
-		const item = [];
 		let title;
 
 		if ( captionEl ) {
@@ -23,31 +52,7 @@ export default function createGallery( el ) {
 			title = '';
 		}
 
-		for ( let s = 0; s < sizes.length; ++s ) {
-			const srcW = sizes[ s ].split( ' ' );
-			const src = srcW[ 0 ];
-			const size = title ? { title } : {};
-
-			if ( src === origImgUrl ) {
-				item.push( Object.assign( size, {
-					src: origImgUrl,
-					w:   imgEl.getAttribute( 'data-ow' ),
-					h:   imgEl.getAttribute( 'data-oh' ),
-					msrc
-				}) );
-
-				continue;
-			}
-
-			const w = srcW[ 1 ].replace( 'w', '' );
-			const xPos = src.lastIndexOf( 'x' );
-			const dotPos = src.lastIndexOf( '.' );
-			const h = src.slice( ( xPos + 1 ), dotPos );
-
-			item.push( Object.assign( size, { msrc, src, w, h }) );
-		}
-
-		gallery.items.push( item );
+		gallery.items.push( createGalleryItem( imgEl, origImgUrl, title ) );
 	}
 
 	gallery = Object.assign({}, gallery, { showCaption });
