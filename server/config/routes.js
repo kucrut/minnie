5fbf574972e063b79797ea2885199be50462a3a9
Dzikri Aziz <kvcrvt@gmail.com>
2018-10-31 20:05:27 +0800

    Server: Update routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,15 +1,12 @@
 /**
- * Routes for express app
+ * Routes for express
  */
-var path = require( 'path' );
-var compiled_app_module_path = path.resolve( __dirname, '../../', 'public', 'assets', 'server.js' );
-var App = require( compiled_app_module_path );
+const path = require( 'path' );
+const compiled_app_module_path = path.resolve( __dirname, '../../', 'public', 'assets', 'server.js' );
+const App = require( compiled_app_module_path );
 
-module.exports = function( app ) {
-	// This is where the magic happens. We take the locals data we have already
-	// fetched and seed our stores with data.
-	// App is a function that requires store data and url to initialize and return the React-rendered html string
-	app.get( '*', function ( req, res ) {
-		App.default( req, res );
-	});
-};
+// This is where the magic happens. We take the locals data we have already
+// fetched and seed our stores with data.
+// App is a function that requires store data and url to initialize and return
+// the React-rendered html string.
+module.exports = app => app.get( '*', ( ...args ) => App.default( ...args ) );
