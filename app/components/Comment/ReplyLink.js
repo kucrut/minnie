48b8bdfb57b4f8959898b1c533234f338c93e67b
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-01 18:35:46 +0800

    components/: Import Link from react-router-dom

--- app/components/Comment/ReplyLink.js
+++ app/components/Comment/ReplyLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function CommentReplyLink( { link } ) {
 	const cls = 'comment-reply-link genericon genericon-reply-single';
--- app/components/Entry/Meta.js
+++ app/components/Entry/Meta.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import he from 'he';
 
 import { entryMetaTaxonomies } from '../../../config/app/config';
