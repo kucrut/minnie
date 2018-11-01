d6c13050b50c34bc939e5823a004c8b6f3ca0795
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-01 18:26:36 +0800

    store/: Clean-up

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,30 +1,16 @@
-/* global __DEVCLIENT__ */
 /* eslint global-require: 0 */
 
 import { createStore, applyMiddleware } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import rootReducer from 'reducers';
-import promiseMiddleware from 'api/promiseMiddleware';
-import createLogger from 'redux-logger';
+
+import promiseMiddleware from '../api/promiseMiddleware';
+import rootReducer from '../reducers';
 
 /*
- * @param {Object} initial state to bootstrap our stores with for server-side rendering
- * @param {History Object} a history object. We use `createMemoryHistory` for server-side rendering,
- *                          while using browserHistory for client-side
- *                          rendering.
+ * @param {Object} initialState Initial state to bootstrap our stores with for server-side rendering.
  */
-export default function configureStore( initialState, history ) {
+export default function configureStore( initialState ) {
 	const middleware = [ thunk, promiseMiddleware ];
-	// Installs hooks that always keep react-router and redux
-	// store in sync
-	const reactRouterReduxMiddleware = routerMiddleware( history );
-	if ( __DEVCLIENT__ ) {
-		middleware.push( reactRouterReduxMiddleware, createLogger() );
-	} else {
-		middleware.push( reactRouterReduxMiddleware );
-	}
-
 	const finalCreateStore = applyMiddleware( ...middleware )( createStore );
 	const store = finalCreateStore( rootReducer, initialState );
 
