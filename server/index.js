bc05ed8651dedb34685ce632ff9273a8fcaa987e
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-02 14:50:47 +0800

    Consilidate server handler

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
+const path = require( 'path' );
 const express = require( 'express' );
 const webpack = require( 'webpack' );
+
+const { port } = require( '../config/app/config' );
+const App = require( '../public/assets/server.js' );
+
 const app = express();
 
-const isDev = process.env.NODE_ENV === 'development';
-
-if ( isDev ) {
-	const config = require( '../config/webpack/dev-client' )( 'development' );
-	const compiler = webpack( config );
+if ( process.env.NODE_ENV === 'development' ) {
+	const webpackConfig = require( '../config/webpack/dev-client' )( 'development' );
+	const compiler = webpack( webpackConfig );
 	const devMiddleware = require( 'webpack-dev-middleware' )( compiler, {
 		noInfo: true,
-		publicPath: config.output.publicPath,
+		publicPath: webpackConfig.output.publicPath,
 	} );
 	const hotMiddleware = require( 'webpack-hot-middleware' )( compiler );
 
@@ -17,8 +20,26 @@ if ( isDev ) {
 	app.use( hotMiddleware );
 }
 
-// Bootstrap application settings
-require( './config/express' )( app );
-require( './config/routes' )( app );
+// Bootstrap application settings.
+app.set( 'port', port );
+// X-Powered-By header has no functional value.
+// Keeping it makes it easier for an attacker to build the site's profile
+// It can be removed safely
+app.disable( 'x-powered-by' );
+app.set( 'view cache', false );
+app.use( express.static( path.join( __dirname, '..', 'public' ) ) );
+
+// Routing.
+// TODO: Check config before disabling favicon.
+app.get( '/favicon.ico', ( req, res ) => res.status( 204 ) );
+app.get( '*', ( ...args ) => App.default( ...args ) );
+
+/* eslint-disable-next-line no-console */
+console.log( `
+----------------------------
+=> Starting Server...'
+=> Environment: ${ process.env.NODE_ENV }
+----------------------------
+` );
 
 app.listen( app.get( 'port' ) );
