68b85852e7480526607fd45de9f35cb3a16d07e4
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-02 22:49:38 +0800

    MenuItem: Convert to stateless component

--- app/components/MenuItem.js
+++ app/components/MenuItem.js
@@ -1,43 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import he from 'he';
 
-export default class MenuItem extends Component {
-	static propTypes = {
-		item: PropTypes.object.isRequired,
-	}
+export default function MenuItem( props ) {
+	const { item } = props;
+	const { children, id, title, url } = item;
+	const idAttr = `menu-item-${ id }`;
+	const text = he.decode( title );
 
-	renderLink( item ) {
-		const title = he.decode( item.title );
-
-		return <NavLink to={ item.url } activeClassName="current-menu-item">{ title }</NavLink>;
-	}
-
-	renderChildren() {
-		const { item } = this.props;
-		const { children } = item;
-
-		if ( ! children.length ) {
-			return null;
-		}
-
-		return (
-			<ul className="sub-menu">
-				{ children.map( child => <MenuItem key={ child.id } item={ child } /> ) }
-			</ul>
-		);
-	}
-
-	render() {
-		const { item } = this.props;
-		const id = `menu-item-${item.id}`;
-
-		return (
-			<li id={ id } className={ `menu-item ${id}` }>
-				{ this.renderLink( item ) }
-				{ this.renderChildren() }
-			</li>
-		);
-	}
+	return (
+		<li id={ idAttr } className={ `menu-item ${ idAttr }` }>
+			<NavLink to={ url } activeClassName="current-menu-item">{ text }</NavLink>
+			{ children.length ? (
+				<ul className="sub-menu">
+					{ children.map( child => <MenuItem key={ child.id } item={ child } /> ) }
+				</ul>
+			) : null }
+		</li>
+	);
 }
+
+MenuItem.propTypes = {
+	item: PropTypes.object.isRequired,
+};
