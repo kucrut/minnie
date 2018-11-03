d941b464acb94257c99aa0a720937c5c059d348f
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-03 21:43:50 +0800

    Clean-up Index page

--- app/pages/Index.js
+++ app/pages/Index.js
@@ -4,12 +4,9 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import he from 'he';
 import { parse } from 'qs';
-import { isEqual } from 'lodash';
 
-import { getAdjacentLink } from '../helpers';
 import { fetchArchive, fetchArchiveTerm } from '../actions/archive';
 import PageHeader from '../components/PageHeader';
-import ContentNavigation from '../components/ContentNavigation';
 import Entry from '../components/Entry/Item';
 import EntryEmpty from '../components/Entry/Empty';
 import Spinner from '../components/Spinner';
@@ -26,78 +23,12 @@ class Index extends Component {
 		} ).isRequired,
 		query: PropTypes.object.isRequired,
 		dispatch: PropTypes.func.isRequired,
-		// route: PropTypes.object.isRequired,
-		// routeParams: PropTypes.object.isRequired,
 	}
 
 	static need = [
 		fetchArchive,
 		fetchArchiveTerm,
 	]
-
-	/**
-	 * Before mount
-	 *
-	 * When this is invoked on the server (the initial page visited is an archive page)
-	 * we need not do anything, because the data will be fetched automatically via
-	 * `fetchComponentDataBeforeRender()`. However, when invoked on the client, we need
-	 * to fetch the data first.
-	 */
-	componentWillMountX() {
-		const { archive, routeParams, query } = this.props;
-		const { isFetching, fetchParams, currentPage } = archive;
-
-		if ( isFetching ) {
-			return;
-		}
-
-		const params = Object.assign( {}, routeParams, query );
-
-		if ( currentPage === 0 || ! isEqual( params, fetchParams ) ) {
-			// this.fetchData( params );
-		}
-	}
-
-	/**
-	 * Before changing page
-	 *
-	 * This is invoked on the client when the visitor transitions between archive pages.
-	 *
-	 * @param  {object} nextProps Next properties.
-	 */
-	componentWillReceivePropsX( nextProps ) {
-		const { archive, routeParams, query } = nextProps;
-		const { isFetching, fetchParams } = archive;
-
-		if ( isFetching ) {
-			return;
-		}
-
-		const params = Object.assign( {}, routeParams, query );
-
-		if ( ! isEqual( params, fetchParams ) ) {
-			this.fetchData( params );
-		}
-	}
-
-	componentDidUpdate( prevProps ) {
-		const { archive, location, match, query } = this.props;
-		const { location: prevLocation } = prevProps;
-		const { isFetching } = archive;
-
-		if ( isFetching ) {
-			return;
-		}
-
-		if ( isEqual( prevLocation, location ) ) {
-			return;
-		}
-
-		this.fetchData( {
-			...match.params,
-			...query,
-		} );
-	}
 
 	fetchData( params ) {
 		const { dispatch } = this.props;
@@ -173,32 +104,6 @@ class Index extends Component {
 		}
 
 		return el;
-	}
-
-	renderNavigation() {
-		const { archive, location, match, query } = this.props;
-
-		if ( ! archive.items.length ) {
-			return null;
-		}
-
-		const { currentPage, hasMore } = archive;
-		const args = {
-			hasMore,
-			currentPage,
-			query,
-			path: location.pathname,
-			params: match.params,
-		};
-
-		const prevLink = getAdjacentLink( false, args );
-		const nextLink = getAdjacentLink( true, args );
-
-		if ( ! ( prevLink || nextLink ) ) {
-			return null;
-		}
-
-		return <ContentNavigation { ...{ prevLink, nextLink } } />;
 	}
 
 	renderEmpty() {
