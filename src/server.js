cc702e180283ce3ea43473482a5e15b59c486aef
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-04 17:14:55 +0800

    Server entry: Use asset paths from manifest

--- src/server.js
+++ src/server.js
@@ -17,11 +17,12 @@ import App from './containers/App';
  * TODO: Add feed URLs <link rel="alternate" href="/feed" type="application/rss+xml" />.
  * TODO: Add sitemap URLs.
  *
+ * @param  {object} manifest     Assets manifest.
  * @param  {string} content      Content.
  * @param  {object} initialState Initial state.
  * @return {string}              Template.
  */
-function createInitialHtml( content, initialState, env = 'production' ) {
+function createInitialHtml( manifest, content, initialState, env = 'production' ) {
 	const { htmlAttributes, title } = Helmet.rewind();
 
 	return `<!doctype html>
@@ -31,17 +32,17 @@ function createInitialHtml( content, initialState, env = 'production' ) {
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		${ title.toString() }
 		<!--link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic|Open+Sans:400,400italic,600,600italic,700,700italic&subset=latin,greek,cyrillic,vietnamese,cyrillic-ext,latin-ext'-->
-		${ env === 'production' ? '<link rel="stylesheet" href="/assets/app.css" />' : '' }
+		${ env === 'production' ? `<link rel="stylesheet" href="${ manifest[ 'main.css' ] }" />` : '' }
 	</head>
 	<body>
 		<div id="app">${ content }</div>
 		<script>window.__INITIAL_STATE__ = ${JSON.stringify( initialState )};</script>
-		<script type="text/javascript" charset="utf-8" src="/assets/app.js"></script>
+		<script type="text/javascript" charset="utf-8" src="${ manifest[ 'main.js' ] }"></script>
 	</body>
 </html>`;
 }
 
-export default function render( env, req, res, next ) {
+export default function render( env, manifest, req, res, next ) {
 	const store = configureStore( {
 		info: { apiUrl },
 	} );
@@ -70,6 +71,7 @@ export default function render( env, req, res, next ) {
 				</Provider>
 			);
 			const markup = createInitialHtml(
+				manifest,
 				renderToString( InitialView ),
 				store.getState(),
 				env,
