11981e8b3fefdc853d6cf59bb4a1f6f0359c88b3
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-04 21:24:46 +0800

    Clean-up fetchInitialData()

--- src/api/fetchInitialData.js
+++ src/api/fetchInitialData.js
@@ -1,28 +1,37 @@
 /**
-* This looks at static needs parameter in components and waits for the promise to be fullfilled
-* It is used to make sure server side rendered pages wait for APIs to resolve before returning
-* res.end().
-* As seen in: https://github.com/caljrimmer/isomorphic-redux-app
-*/
+ * Fetch Initial Data
+ *
+ * This looks at static `needs` parameter in components and waits for the promise
+ * to be fullfilled. It is used to make sure server side rendered pages wait for
+ * APIs to resolve before returning res.end().
+ * As seen in: https://github.com/caljrimmer/isomorphic-redux-app
+ *
+ * @param {function} dispatch   Store's dispatch function.
+ * @param {array}    components Components to render for initial view.
+ * @param {object}   params     Fetch parameters.
+ *
+ * @return Promise
+ */
 
 export default function fetchInitialData( dispatch, components, params ) {
-	// Commenting this out since it's producing duplicates.
-	/*
-	const needs = components.reduce( (prev, current) => {
-		return (current.need || [])
-			.concat((current.WrappedComponent ? current.WrappedComponent.need : []) || [])
-			.concat(prev);
-	}, []);
-	*/
+	const needs = components.reduce( ( all, current ) => {
+		let result = [ ...all ];
 
-	const needs = components.reduce( ( prev, current ) => {
-		let more = [];
-
-		if ( current.need && current.WrappedComponent ) {
-			more = current.WrappedComponent.need;
+		if ( current.need ) {
+			result = [
+				...result,
+				...current.need,
+			];
 		}
 
-		return more.concat( prev );
+		if ( current.WrappedComponent && current.WrappedComponent.need ) {
+			result = [
+				...result,
+				...current.WrappedComponent.need,
+			];
+		}
+
+		return result;
 	}, [] );
 
 	const promises = needs.map( need => dispatch( need( params ) ) );
