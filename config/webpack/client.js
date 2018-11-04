1e0ed99ba0aea374182c3cc264e119069ba90bae
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-04 20:07:01 +0800

    Webpack client config: Only include hot middleware on dev

--- config/webpack/client.js
+++ config/webpack/client.js
@@ -3,26 +3,30 @@ const webpack = require( 'webpack' );
 const merge = require( 'webpack-merge' );
 const getSharedConfig = require( './shared' );
 
-module.exports = env => merge( getSharedConfig( env ), {
-	name: `browser-${ env }`,
-	// Multiple entry with hot loader
-	// https://github.com/glenjamin/webpack-hot-middleware/blob/master/example/webpack.config.multientry.js
-	output: {
-		path: path.join( process.cwd(), 'public', 'client', 'assets' ),
-		filename: '[name].[hash].js',
-		publicPath: '/assets/',
-	},
-	entry: {
-		main: [
-			'./src/client',
-			'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
+module.exports = env => {
+	const isProduction = env === 'production';
+
+	return merge( getSharedConfig( env ), {
+		name: `browser-${ env }`,
+		// Multiple entry with hot loader
+		// https://github.com/glenjamin/webpack-hot-middleware/blob/master/example/webpack.config.multientry.js
+		output: {
+			path: path.join( process.cwd(), 'public', 'client', 'assets' ),
+			filename: '[name].[hash].js',
+			publicPath: '/assets/',
+		},
+		entry: {
+			main: [
+				'./src/client',
+				( ! isProduction && 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true' ),
+			].filter( Boolean ),
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.DefinePlugin( {
+				__DEVCLIENT__: ! isProduction,
+				__DEVSERVER__: false,
+			} ),
 		],
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.DefinePlugin( {
-			__DEVCLIENT__: env === 'development',
-			__DEVSERVER__: false,
-		} ),
-	],
-} );
+	} );
+}
