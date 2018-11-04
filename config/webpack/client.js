062a7d1d1f34a43fece3ae9d3c23d4a91a759f91
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-04 17:12:49 +0800

    Webpack: Rename client entry output to `main`

--- config/webpack/client.js
+++ config/webpack/client.js
@@ -7,7 +7,7 @@ module.exports = env => merge( getSharedConfig( env ), {
 	// Multiple entry with hot loader
 	// https://github.com/glenjamin/webpack-hot-middleware/blob/master/example/webpack.config.multientry.js
 	entry: {
-		app: [
+		main: [
 			'./src/client',
 			'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
 		],
