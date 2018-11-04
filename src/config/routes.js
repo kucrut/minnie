562e210119a5ac736793d037b5d05ee4d61b2ab7
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-05 00:18:25 +0800

    Routes: Accept taxonomies array to create term archive routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,3 @@
-import { taxonomyMap } from '../config/app';
 import Login from '../pages/Login';
 import Index from '../pages/Index';
 import Page from '../pages/Page';
@@ -6,58 +5,85 @@ import Post from '../pages/Post';
 import Media from '../pages/Media';
 import Preview from '../pages/Preview';
 
-export default [].concat(
-	[
-		{
-			path: '/',
-			exact: true,
-			component: Index,
-		},
-		{
-			path: '/login',
-			component: Login,
-		},
-	],
-	Object.keys( taxonomyMap ).map( tax => {
-		// TODO: Refactor taxonomyMap.
-		const route = tax === 'format' ? 'type' : tax;
+function createTermRoutes( taxonomies ) {
+	return taxonomies.map( tax => {
+		const { slug, types } = tax;
+
+		if ( ! types.includes( 'post' ) ) {
+			return null;
+		}
+
+		let route;
+
+		switch ( slug ) {
+			case 'category':
+				route = slug;
+				break;
+
+			case 'post_tag':
+				route = 'tag';
+				break;
+
+			case 'post_format':
+				route = 'type';
+				break;
+			default:
+				route = slug;
+		}
 
 		return {
-			path: `/blog/${ route }/:${ tax }`,
+			path: `/blog/${ route }/:${ slug }`,
 			component: Index,
 		};
-	 } ),
-	[
-		{
-			path: '/blog/:slug',
-			component: Post,
-		},
-		{
-			path: '/page/:page',
-			component: Index,
-		},
-		{
-			path: '/:parentslug/:slug',
-			component: Page,
-		},
-		{
-			path: '/blog/:parentslug/:slug',
-			component: Media,
-		},
-		{
-			path: '/preview/:type/:id',
-			component: Preview,
-		},
-		{
-			path: '/:slug([^\\?]+)',
-			component: Page,
-		},
-	],
-	// TODO: Add more routes (archive, paged).
-	[
-		{
-			path: '*',
-			component: Index,
-		},
-	],
-);
+	} ).filter( Boolean );
+}
+
+export default function createRoutes( taxonomies ) {
+	return [].concat(
+		[
+			{
+				path: '/',
+				exact: true,
+				component: Index,
+			},
+			{
+				path: '/login',
+				component: Login,
+			},
+		],
+		createTermRoutes( taxonomies ),
+		[
+			{
+				path: '/blog/:slug',
+				component: Post,
+			},
+			{
+				path: '/page/:page',
+				component: Index,
+			},
+			{
+				path: '/:parentslug/:slug',
+				component: Page,
+			},
+			{
+				path: '/blog/:parentslug/:slug',
+				component: Media,
+			},
+			{
+				path: '/preview/:type/:id',
+				component: Preview,
+			},
+			{
+				path: '/:slug([^\\?]+)',
+				component: Page,
+			},
+		],
+		// TODO: Add more routes (archive, paged).
+		[
+			{
+				path: '*',
+				component: Index,
+			},
+		],
+	);
+}
