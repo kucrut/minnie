79d52113ff5b5992710e3b9d69b351ef75863631
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-04 17:14:01 +0800

    Server handler: Grab & pass assets manifest to server entry

--- server/index.js
+++ server/index.js
@@ -8,8 +8,13 @@ const App = require( '../public/assets/server.js' );
 
 const app = express();
 const env = process.env.NODE_ENV;
+const manifestPath = '../public/assets/client-manifest.json';
+let manifest;
 
-if ( env === 'development' ) {
+if ( env === 'production' ) {
+	// On production, the manifest is already created, so just require() it.
+	manifest = require( manifestPath );
+} else {
 	const webpackConfig = require( '../config/webpack/client' )( 'development' );
 	const compiler = webpack( webpackConfig );
 	const devMiddleware = require( 'webpack-dev-middleware' )( compiler, {
@@ -20,6 +25,10 @@ if ( env === 'development' ) {
 
 	app.use( devMiddleware );
 	app.use( hotMiddleware );
+	app.use( ( req, res, next ) => {
+		manifest = require( manifestPath );
+		next();
+	} );
 }
 
 // Bootstrap application settings.
@@ -35,7 +44,7 @@ app.use( express.static( path.join( __dirname, '..', 'public' ) ) );
 // Routing.
 // TODO: Check config before disabling favicon.
 app.get( '/favicon.ico', ( req, res ) => res.status( 204 ) );
-app.get( '*', ( ...args ) => App.default( env, ...args ) );
+app.get( '*', ( ...args ) => App.default( env, manifest, ...args ) );
 
 /* eslint-disable-next-line no-console */
 console.log( `
