ef686b4363a28bf353a7136b37972e290fd3b12f
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-06 22:44:54 +0800

    Singular: Use Main

--- src/containers/Singular/index.js
+++ src/containers/Singular/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import he from 'he';
-import Helmet from 'react-helmet';
 
+import Main from '../Main';
 import NotFound from '../../pages/404';
-import Entry from '../../components/Entry/Item';
 import Spinner from '../../components/Spinner';
 
 export default function Singular( props ) {
@@ -12,29 +11,19 @@ export default function Singular( props ) {
 	let title;
 
 	if ( isFetching ) {
-		return ( <Spinner /> );
+		return <Spinner />;
 	}
 
 	if ( ! data.id ) {
-		return ( <NotFound /> );
+		return <NotFound />;
 	}
 
 	title = data.title.rendered ? data.title.rendered : data.title.from_content;
 	title = he.decode( title );
 
 	return (
-		<div className="content">
-			<Helmet
-				title={ title }
-				titleTemplate={ `%s | ${info.name}` }
-			/>
-
-			<div id="primary" className="content-area">
-				<main id="main" className="site-main" role="main">
-					<Entry data={ data } isSingle={ true } />
-					{ children }
-				</main>
-			</div>
-		</div>
+		<Main title={ title } titleTemplate={ `%s | ${info.name}` }>
+			{ children }
+		</Main>
 	);
 }
