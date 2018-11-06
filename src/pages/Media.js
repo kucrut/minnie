c6fb6c727f162acaa942ee12548838c3219554eb
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-06 22:51:07 +0800

    Media: Pass Entry child to Singular

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -4,6 +4,7 @@ import { fetchMedia } from '../store/actions/singular';
 import withSingularData from '../higher-order/with-singular-data';
 import Singular from '../containers/Singular';
 import ContentNavigation from '../components/ContentNavigation';
+import Entry from '../components/Entry/Item';
 
 function mapStateToProps( state ) {
 	return {
@@ -27,8 +28,9 @@ function Media( props ) {
 
 	return (
 		<Singular { ...props }>
+			<Entry isSingle data={ data } />
 			<ContentNavigation
-				isSingle={ true }
+				isSingle
 				prevLink={ parent_post.link }
 				prevText={ parent_post.title.rendered }
 			/>
