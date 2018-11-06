f5793f25c44c8b946ef30c3ca91f473271f748ec
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-06 08:02:47 +0800

    Page: Import methods from _Singular

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,24 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import he from 'he';
+import Helmet from 'react-helmet';
+
+import NotFound from './404';
+import Spinner from '../components/Spinner';
+import Entry from '../components/Entry/Item';
 
 import { fetchPage } from '../store/actions/singular';
-import _Singular from './_Singular';
 
-class Page extends _Singular {
+class Page extends Component {
 
-	/**
-	 * Callbacks needed for server-side rendering
-	 *
-	 * Functions listed here will be called automatically by `fetchComponentDataBeforeRender()`
-	 *     when this component is rendered by the server.
-	 *
-	 * @type {Array}
-	 */
 	static need = [ fetchPage ]
 
-	static displayName = 'Page'
+	static propTypes = {
+		info: PropTypes.object.isRequired,
+		singular: PropTypes.object.isRequired,
+		slug: PropTypes.string.isRequired,
+	}
 
-	fetchData( slug ) {
-		this.props.dispatch( fetchPage( { slug } ) );
+	componentDidMount() {
+		const { slug, singular } = this.props;
+		const { data } = singular;
+
+		if ( data.slug !== slug ) {
+			this.fetchData();
+		}
+	}
+
+	componentDidUpdate( prevProps ) {
+		const { slug, singular } = this.props;
+		const { isFetching } = singular;
+
+		if ( isFetching ) {
+			return;
+		}
+
+		if ( prevProps.slug !== slug ) {
+			this.fetchData();
+		}
+	}
+
+	fetchData() {
+		const { dispatch, slug } = this.props;
+		dispatch( fetchPage( { slug } ) );
+	}
+
+	render() {
+		const { info, singular } = this.props;
+		const { data, isFetching } = singular;
+		let title;
+
+		if ( isFetching ) {
+			return ( <Spinner /> );
+		}
+
+		if ( ! data.id ) {
+			return ( <NotFound /> );
+		}
+
+		title = data.title.rendered ? data.title.rendered : data.title.from_content;
+		title = he.decode( title );
+
+		return (
+			<div className="content">
+				<Helmet
+					title={ title }
+					titleTemplate={ `%s | ${info.name}` }
+				/>
+
+				<div id="primary" className="content-area">
+					<main id="main" className="site-main" role="main">
+						<Entry data={ data } isSingle={ true } />
+						{/* this.renderNavigation() */}
+						{/* this.renderComments() */}
+					</main>
+				</div>
+			</div>
+		);
 	}
 }
 
@@ -26,8 +86,8 @@ export function mapStateToProps( state, ownProps ) {
 	return {
 		info: state.info,
 		singular: state.singular,
-		user: state.session.user,
 		slug: ownProps.match.params.slug,
+		// user: state.session.user,
 	};
 }
 
