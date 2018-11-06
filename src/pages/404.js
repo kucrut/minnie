2075e97cf11a3692938b4bbdf18fba7d6e8ea375
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-06 22:58:45 +0800

    404 page: Use Main

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
+import Main from '../containers/Main';
 import EntryEmpty from '../components/Entry/Empty';
 
 export default function NotFound() {
 	return (
-		<div className="content">
-			<Helmet title="Oops!" />
-
-			<div id="primary" className="content-area">
-				<main id="main" className="site-main" role="main">
-					<EntryEmpty
-						title="Oops! That page can&rsquo;t be found."
-						content="It looks like nothing was found at this location. Maybe try a search?"
-					/>
-				</main>
-			</div>
-		</div>
+		<Main title="Oops!">
+			<EntryEmpty
+				title="Oops! That page can&rsquo;t be found."
+				content="It looks like nothing was found at this location. Maybe try a search?"
+			/>
+		</Main>
 	);
 }
