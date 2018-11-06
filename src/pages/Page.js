4666a1482a5b6073cfe8d7fc1ac72a30dcc831ab
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-06 17:28:09 +0800

    Page: Use Singular container

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,46 +1,7 @@
-import React from 'react';
-import he from 'he';
-import Helmet from 'react-helmet';
-
-import withSingular from '../hoc/with-singular';
-import NotFound from './404';
-import Spinner from '../components/Spinner';
-import Entry from '../components/Entry/Item';
+import withSingularData from '../higher-order/with-singular-data';
+import Singular from '../containers/Singular';
 
 import { fetchPage } from '../store/actions/singular';
-
-function Page( props ) {
-	const { info, singular } = props;
-	const { data, isFetching } = singular;
-	let title;
-
-	if ( isFetching ) {
-		return ( <Spinner /> );
-	}
-
-	if ( ! data.id ) {
-		return ( <NotFound /> );
-	}
-
-	title = data.title.rendered ? data.title.rendered : data.title.from_content;
-	title = he.decode( title );
-
-	return (
-		<div className="content">
-			<Helmet
-				title={ title }
-				titleTemplate={ `%s | ${info.name}` }
-			/>
-
-			<div id="primary" className="content-area">
-				<main id="main" className="site-main" role="main">
-					<Entry data={ data } isSingle={ true } />
-					{/* TODO: Navigation */}
-				</main>
-			</div>
-		</div>
-	);
-}
 
 const fetchData = props => {
 	const { dispatch, slug } = props;
@@ -51,7 +12,11 @@ const fetchData = props => {
 	dispatch( fetchPage( args ) );
 }
 
-export default withSingular( {
+const Page = withSingularData( {
 	fetchData,
 	need: [ fetchPage ],
-} )( Page );
+} )( Singular );
+
+Page.displayName = 'Page';
+
+export default Page;
