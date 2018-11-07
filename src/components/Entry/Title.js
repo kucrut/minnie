4d8cb26f0ea54ae0eb85dedab994a7f34932c513
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-07 18:14:03 +0800

    EntryTitle: Rewrite

--- src/components/Entry/Title.js
+++ src/components/Entry/Title.js
@@ -1,32 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import he from 'he';
+import { decode } from 'he';
 
-export default class EntryTitle extends Component {
-	static propTypes = {
-		link: PropTypes.string,
-		title: PropTypes.string.isRequired,
-		isSingle: PropTypes.bool.isRequired,
-	}
+export default function EntryTitle( props ) {
+	const { link, title } = props;
+	const text = decode( title );
 
-	render() {
-		const { isSingle, link, title } = this.props;
-		const titleText = he.decode( title );
-		let el;
-
-		if ( isSingle ) {
-			el = (
-				<h1 className="entry-title">{ titleText }</h1>
-			);
-		} else {
-			el = (
-				<h1 className="entry-title">
-					<Link to={ link }>{ titleText }</Link>
-				</h1>
-			);
-		}
-
-		return el;
-	}
+	return (
+		<h1 className="entry-title">
+			{ link ? <Link to={ link }>{ text }</Link> : text }
+		</h1>
+	);
 }
+
+EntryTitle.defaultProps = {
+	link: '',
+};
+
+EntryTitle.propTypes = {
+	title: PropTypes.string.isRequired,
+	link: PropTypes.string,
+};
