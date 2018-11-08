ce9a30922e664ff3bc61a4474adbac5a5c4c8c8b
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 22:25:28 +0800

    Add helpers for EntryContent

---
+++ src/components/Entry/Content/utils.js
@@ -0,0 +1,18 @@
+import { siteUrl } from '../../../config';
+
+export function isInternalUrl( url ) {
+	if ( url.indexOf( siteUrl ) !== 0 ) {
+		return false;
+	}
+
+	// TODO: Add /wp-content to config.
+	if ( url.indexOf( `${ siteUrl }/wp-content` ) === 0 ) {
+		return false;
+	}
+
+	return true;
+}
+
+export function stripSiteUrl( url ) {
+	return url.replace( siteUrl, '' );
+}
