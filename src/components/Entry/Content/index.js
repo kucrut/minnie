b9ce5c4ddaf1e98aa47e88bae641e794b41fc381
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 22:33:04 +0800

    EntryContent: Unwrap off withRouter

--- src/components/Entry/Content/index.js
+++ src/components/Entry/Content/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Interweave from 'interweave';
-import { withRouter } from 'react-router-dom';
 
 import transformer from './transformer';
 
-class EntryContent extends Component {
+export default class EntryContent extends Component {
 	static defaultProps = {
 		className: 'entry-content',
 	};
@@ -29,5 +28,3 @@ class EntryContent extends Component {
 		);
 	}
 }
-
-export default withRouter( EntryContent );
