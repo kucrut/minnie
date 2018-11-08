fb8df0d592db5afcd730e30f324838b1bfab5c7a
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 22:29:52 +0800

    EntryContent: Remove obsolete click handler

--- src/components/Entry/Content/index.js
+++ src/components/Entry/Content/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import closest from 'dom-closest';
 import Interweave from 'interweave';
 import { withRouter } from 'react-router-dom';
 
-import { contentPathRegEx } from '../../../helpers';
 import transformer from './transformer';
 
 class EntryContent extends Component {
@@ -16,28 +14,6 @@ class EntryContent extends Component {
 		content: PropTypes.string.isRequired,
 		className: PropTypes.string,
 	};
-
-	handleClick( e ) {
-		const { history } = this.props;
-		const anchor = closest( e.target, 'a' );
-
-		if ( ! anchor ) {
-			return;
-		}
-
-		// Don't bother if this is an external link.
-		if ( anchor.hostname !== window.location.hostname ) {
-			return;
-		}
-
-		// Don't bother if this is a link to an attachment file.
-		if ( contentPathRegEx.test( anchor.pathname ) ) {
-			return;
-		}
-
-		e.preventDefault();
-		history.push( anchor.pathname );
-	}
 
 	render() {
 		const { content, className } = this.props;
