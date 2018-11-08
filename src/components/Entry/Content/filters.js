4c38304200b0924e05681acd6dc0f34f61f8ed2f
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 21:34:00 +0800

    Content filter: InternalLinkFilter

---
+++ src/components/Entry/Content/filters.js
@@ -0,0 +1,35 @@
+import { Filter } from 'interweave';
+import { trim } from 'lodash';
+
+import { siteUrl } from '../../../config';
+
+export function isInternalUrl( url ) {
+	if ( url.indexOf( siteUrl ) !== 0 ) {
+		return false;
+	}
+
+	// TODO: Add /wp-content to config.
+	if ( url.indexOf( `${ siteUrl }/wp-content` ) === 0 ) {
+		return false;
+	}
+
+	return true;
+}
+
+class InternalLinkFilter extends Filter {
+	attribute( name, value ) {
+		if ( ! isInternalUrl( value ) ) {
+			return value;
+		}
+
+		let newValue = value.replace( siteUrl, '' );
+		newValue = trim( newValue, '/' );
+		newValue = `/${ newValue }`;
+
+		return newValue;
+	}
+}
+
+export default [
+	InternalLinkFilter,
+];
