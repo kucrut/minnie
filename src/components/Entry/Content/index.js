831fa1ed0b9f6a092c4daa3a43b5d03fe8bdcdec
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 22:35:44 +0800

    EntryContent: Convert to functional component

--- src/components/Entry/Content/index.js
+++ src/components/Entry/Content/index.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Interweave from 'interweave';
 
 import transformer from './transformer';
 
-export default class EntryContent extends Component {
-	static defaultProps = {
-		className: 'entry-content',
-	};
+export default function EntryContent( props ) {
+	const { content, className } = props;
 
-	static propTypes = {
-		content: PropTypes.string.isRequired,
-		className: PropTypes.string,
-	};
-
-	render() {
-		const { content, className } = this.props;
-
-		return (
-			<Interweave
-				className={ className }
-				commonClass={ null }
-				content={ content }
-				tagName="div"
-				transform={ transformer }
-			/>
-		);
-	}
+	return (
+		<Interweave
+			className={ className }
+			commonClass={ null }
+			content={ content }
+			tagName="div"
+			transform={ transformer }
+		/>
+	);
 }
+
+EntryContent.defaultProps = {
+	className: 'entry-content',
+};
+
+EntryContent.propTypes = {
+	content: PropTypes.string.isRequired,
+	className: PropTypes.string,
+};
