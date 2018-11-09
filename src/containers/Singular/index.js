5d95fd9a4306c6c35742addd4746e8b0bdc110ee
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 00:25:42 +0800

    Singular: Remove parts already handled by withSingular()

--- src/containers/Singular/index.js
+++ src/containers/Singular/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import he from 'he';
 
 import Main from '../Main';
-import NotFound from '../../pages/404';
-import Spinner from '../../components/Spinner';
 
 export default function Singular( props ) {
 	const { info, singular, children } = props;
-	const { data, isFetching } = singular;
+	const { data } = singular;
 	let title;
-
-	if ( isFetching ) {
-		return <Spinner />;
-	}
-
-	if ( ! data.id ) {
-		return <NotFound />;
-	}
 
 	title = data.title.rendered ? data.title.rendered : data.title.from_content;
 	title = he.decode( title );
