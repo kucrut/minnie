a2774d23bacb677ff6688db0a331870c1f2ace87
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-09 21:34:02 +0800

    Clean-up singular reducer

--- src/store/reducers/singular.js
+++ src/store/reducers/singular.js
@@ -1,58 +1,56 @@
-import { head } from 'lodash';
-
 import {
 	GET_SINGULAR_REQUEST,
 	GET_SINGULAR_SUCCESS,
 	GET_SINGULAR_FAILURE,
 } from '../constants';
 
-const initialTitle = 'Oops! That page can&rsquo;t be found.';
 const initialState = {
 	data: {},
+	error: null,
 	isFetching: false,
 };
 
 export default function singular( state = initialState, action ) {
-	let data;
-	let slug;
-
 	switch ( action.type ) {
-		case GET_SINGULAR_REQUEST:
-			return Object.assign( {}, initialState, {
+		case GET_SINGULAR_REQUEST: {
+			return {
+				...state,
 				isFetching: true,
-			} );
+			};
+		}
 
-		case GET_SINGULAR_SUCCESS:
-			if ( action.isPreview ) {
-				data = action.req.data;
-				slug = `preview-${action.postId}`;
+		case GET_SINGULAR_SUCCESS: {
+			const { data } = action.req;
+			let newData;
+
+			if ( data.id ) {
+				// Preview.
+				newData = data;
+			} else if ( data.length ) {
+				// Singular post/page.
+				newData = data[0];
 			} else {
-				data = head( action.req.data, 1 );
-				slug = action.req.config.params.slug;
+				// Not found; the API returns 200 with empty set of result.
+				newData = initialState.data;
 			}
 
-			/**
-		 * Page/post not found.
-		 *
-		 * We need to store the `slug` so the component will know
-		 * that we've already requested the page.
-		 */
-			if ( ! data ) {
-				data = {
-					title: initialTitle,
-					slug,
-				};
-			}
-
-			return Object.assign( {}, state, {
+			return {
+				...state,
+				data: newData,
+				error: null,
 				isFetching: false,
-				data,
-			} );
+			}
+		}
 
-		case GET_SINGULAR_FAILURE:
-			return Object.assign( {}, initialState );
+		case GET_SINGULAR_FAILURE: {
+			return {
+				...initialState,
+				error: action.error,
+			};
+		}
 
-		default:
+		default: {
 			return state;
+		}
 	}
 }
