9ac93589b0ea80823e238769f14ef2cb13d795ce
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 00:32:55 +0800

    404 page: Handle initial status code

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
+import { Route } from 'react-router-dom';
+
 import Main from '../../containers/Main';
 import NoContent from '../../components/NoContent';
 
-function NotFound() {
-	return (
-		<Main title="Oops!">
-			<section className="error-404 not-found">
-				<header className="page-header">
-					<h1 className="page-title">Oops! That page can&rsquo;t be found.</h1>
-				</header>
+const Oops = () => (
+	<Main title="Oops!">
+		<section className="error-404 not-found">
+			<header className="page-header">
+				<h1 className="page-title">Oops! That page can&rsquo;t be found.</h1>
+			</header>
 
-				<NoContent text="It looks like nothing was found at this location. Maybe try a search?" />
-			</section>
-		</Main>
-	);
+			<NoContent text="It looks like nothing was found at this location. Maybe try a search?" />
+		</section>
+	</Main>
+);
+
+const NotFoundWithRoute = ( { status, children } ) => (
+	<Route
+		render={ ( { staticContext } ) => {
+			if ( staticContext ) {
+				staticContext.status = status;
+			}
+
+			return children;
+		} }
+	/>
+);
+
+function NotFound( { status } ) {
+	if ( status ) {
+		return (
+			<NotFoundWithRoute status={ status }>
+				<Oops />
+			</NotFoundWithRoute>
+		);
+	}
+
+	return <Oops />;
 }
 
 export default hot( module )( NotFound );
