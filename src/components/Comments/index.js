9e9ac638683ec68c5684d116b05ab9a3cd34a816
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 11:37:31 +0800

    Add base Comments component

---
+++ src/components/Comments/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Comments() {
+	return (
+		<div className="comments-area" id="comments">
+			<h2 className="comments-title">Comments</h2>
+		</div>
+	);
+}
