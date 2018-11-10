679060be3c33decb8b9623f1b24d3dcccae2d88b
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 11:35:12 +0800

    Start refactoring comments reducer

--- src/store/reducers/comments.js
+++ src/store/reducers/comments.js
@@ -11,138 +11,61 @@ const onHoldText = 'Your comment is waiting for moderation.';
 const dupeText = 'Duplicate comment detected; it looks as though you’ve already said that!';
 
 const initialThreadState = {
-	parentId: 0,
-	items: [],
-	hasMore: false,
 	currentPage: 0,
+	hasMore: false,
 	isFetching: false,
+	items: [],
+	parentId: 0,
 };
 
 const initialState = {
-	newComment: {},
-	isSubmitting: false,
-	hasError: false,
-	error: {},
+	error: null,
+	isPosting: false,
+	newComment: null,
 	postId: 0,
-	threads: {
-		t0: Object.assign( {}, initialThreadState ),
-	},
+	threads: [],
 };
 
-function getThread( state, parentId ) {
-	const threadId = `t${parentId}`;
-
-	if ( threadId in state.threads ) {
-		return state.threads[ threadId ];
-	}
-
-	return Object.assign( {}, initialThreadState, {
-		parentId,
-	} );
-}
-
 export default function comments( state = initialState, action ) {
-	const { postId, parentId } = action;
-	const threadId = `t${parentId}`;
-	const threadState = getThread( state, parentId );
-
-	let threads;
-	let params;
-	let currentPage;
-	let items;
-	let newComment;
-	let error;
-
 	switch ( action.type ) {
-		case GET_COMMENTS_REQUEST:
-			if ( postId === state.postId ) {
-				threads = Object.assign( {}, state.threads, {
-					[ threadId ]: Object.assign( {}, threadState, { isFetching: true } ),
-				} );
-			} else {
-				threads = Object.assign( {}, {
-					[ threadId ]: Object.assign( {}, threadState, { isFetching: true } ),
-				} );
-			}
+		case GET_COMMENTS_REQUEST: {
+			return {
+				...state,
+			};
+		}
 
-			return Object.assign( {}, state, {
-				postId,
-				threads,
-			} );
+		case GET_COMMENTS_SUCCESS: {
+			return {
+				...state,
+			};
+		}
 
-		case GET_COMMENTS_SUCCESS:
-			params      = action.req.config.params;
-			currentPage = parseInt( params.page, 10 ) || 1;
+		case GET_COMMENTS_FAILURE: {
+			return {
+				...state,
+			};
+		}
 
-			if ( currentPage > 1 ) {
-				items = threadState.items.concat( action.req.data );
-			} else {
-				items = action.req.data;
-			}
+		case POST_COMMENT_REQUEST: {
+			return {
+				...state,
+			};
+		}
 
-			return Object.assign( {}, state, {
-				threads: Object.assign( {}, state.threads, {
-					[ threadId ]: Object.assign( {}, threadState, {
-						hasMore: currentPage < action.req.headers[ 'x-wp-totalpages' ],
-						isFetching: false,
-						currentPage,
-						items,
-					} ),
-				} ),
-			} );
+		case POST_COMMENT_SUCCESS: {
+			return {
+				...state,
+			};
+		}
 
-		case GET_COMMENTS_FAILURE:
-			return Object.assign( {}, state, {
-				threads: Object.assign( {}, state.threads, {
-					[ threadId ]: Object.assign( {}, threadState, {
-						isFetching: false,
-					} ),
-				} ),
-			} );
+		case POST_COMMENT_FAILURE: {
+			return {
+				...state,
+			};
+		}
 
-		case POST_COMMENT_REQUEST:
-			return Object.assign( {}, state, {
-				isSubmitting: true,
-				hasError: false,
-				error: {},
-			} );
-
-		case POST_COMMENT_SUCCESS:
-			newComment = Object.assign( {}, action.req.data );
-
-			if ( newComment.status !== 'approved' ) {
-				newComment = Object.assign( {}, newComment, {
-					content: Object.assign( {}, newComment.content, {
-						original: newComment.content.rendered,
-						rendered: `<p class="comment-awaiting-moderation">${onHoldText}</p>`,
-					} ),
-				} );
-			}
-
-			items = threadState.items.concat( [ newComment ] );
-
-			return Object.assign( {}, state, {
-				newComment,
-				isSubmitting: false,
-				threads: Object.assign( {}, state.threads, {
-					[ threadId ]: Object.assign( {}, threadState, { items } ),
-				} ),
-			} );
-
-		case POST_COMMENT_FAILURE:
-			if ( action.error.status === 409 ) {
-				error = { duplicate: dupeText };
-			} else {
-				error = action.error.data.data.params;
-			}
-
-			return Object.assign( {}, state, {
-				isSubmitting: false,
-				hasError: true,
-				error,
-			} );
-
-		default:
+		default: {
 			return state;
+		}
 	}
 }
