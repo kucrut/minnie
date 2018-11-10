3f93a43a5e3e4979d0df1a6fb5ec642425ea0662
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 16:14:17 +0800

    Add thread to Comments' mapped props

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -15,21 +15,25 @@ Comments.propTypes = {
 	isOpen: PropTypes.bool.isRequired,
 	maxDepth: PropTypes.number.isRequired,
 	postId: PropTypes.number.isRequired,
+	thread: PropTypes.object.isRequired,
 	threadId: PropTypes.number.isRequired,
 };
 
-const mapStateToProps = state => {
-	const { info, singular } = state;
+const mapStateToProps = ( state, ownProps ) => {
+	const { info, comments, singular } = state;
 	const { data } = singular;
 	const { comment_status, id } = data;
 	const { settings } = info;
-	const { comments } = settings;
-	const { threads_depth } = comments;
+	const { comments: commentsSettings } = settings;
+	const { threads_depth } = commentsSettings;
+	const { threadId } = ownProps;
+	const { threads } = comments;
 
 	return {
 		isOpen: comment_status === 'open',
 		maxDepth: threads_depth,
 		postId: id,
+		thread: threads.find( current => current.parentId === threadId ) || null,
 	};
 };
 
