b301eecf0bd2bf9de7df9ddc5b76f7d50ffc38e4
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-10 11:52:46 +0800

    Comments: Connect to state

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Comments() {
+function Comments() {
 	return (
 		<div className="comments-area" id="comments">
 			<h2 className="comments-title">Comments</h2>
 		</div>
 	);
 }
+
+const mapStateToProps = state => {
+	const { info, singular } = state;
+	const { data } = singular;
+	const { comment_status, id } = data;
+	const { settings } = info;
+	const { comments } = settings;
+	const { threads_depth } = comments;
+
+	return {
+		maxDepth: threads_depth,
+		postId: id,
+		status: comment_status,
+	};
+};
+
+export default connect( mapStateToProps )( Comments );
