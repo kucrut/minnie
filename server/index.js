3adddd550dd80bb8453bbf35b5fe4bbe6899f3e8
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-11 23:45:11 +0800

    Server handler: Set fallback for entryMetaTaxonomies

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = parseInt( process.env.PORT, 10 ) || 9000;
 const appConfig = {
 	env,
 	siteUrl: process.env.WP_URL,
-	entryMetaTaxonomies: process.env.ENTRY_META_TAX.split( ',' ),
+	entryMetaTaxonomies: process.env.ENTRY_META_TAX.split( ',' ) || [],
 };
 
 const manifestPath = '../public/client/assets/manifest.json';
