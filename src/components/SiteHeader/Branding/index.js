0a72ef2e39cba30aefe9467c0dd4fca4a10cbe25
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-13 00:58:34 +0800

    Branding: Decode site name

--- src/components/SiteHeader/Branding/index.js
+++ src/components/SiteHeader/Branding/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { decode } from 'he';
 
 export default function Branding( { name } ) {
 	return (
 		<div className="site-branding">
-			<h1 className="site-title"><Link to="/">{ name }</Link></h1>
+			<h1 className="site-title"><Link to="/">{ decode( name ) }</Link></h1>
 		</div>
 	);
 }
